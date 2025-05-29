2dc8097d791838cb71ff3d173d7b769beba9985a
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-05-29 19:55:15 +0200

    commented wrong routes for deploying

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Revenue } from './definitions';
+import { Revenue } from '@/generated/prisma-client';
 
 export const formatCurrency = (amount: number) => {
   return (amount / 100).toLocaleString('en-US', {
