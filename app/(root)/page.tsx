c60d8e2dfca394a6d2851e7d3f00614bbdcae7e0
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-05-30 22:31:01 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Daniel", lastName: 'Pérez', email: 'contact@saldo.com' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effectively"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{ currentBalance: 123.50 }, {currentBalance: 500}]}
+      />
+    </section>
+  );
 };
 
 export default Home;
